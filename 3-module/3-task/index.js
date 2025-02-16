80530b084cdefa5b6b99a5484346236a928df1bb
Sergey Bova <bova_s@goldapple.ru>
2025-02-16 16:03:53 +0500

    solution until 3 module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,8 @@
 function camelize(str) {
   // ваш код...
+  let arr = str.split("-");
+  let modify = arr
+    .slice(1)
+    .map((item) => `${item.charAt(0).toUpperCase()}${item.slice(1)}`);
+  return arr.slice(0, 1).concat(modify).join("");
 }
