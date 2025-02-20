9999764c98d025505a3a5ba4df80adb24a08e5da
Sergey Bova <bova_s@goldapple.ru>
2025-02-20 19:06:31 +0500

    solution until 4 module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
   // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    if (row.cells[3].hasAttribute("data-available")) {
+      if (row.cells[3].dataset.available == "true") {
+        row.classList.add("available");
+      } else {
+        row.classList.add("unavailable");
+      }
+    } else {
+      row.hidden = true;
+    }
+    if (row.cells[2].innerText === "m") {
+      row.classList.add("male");
+    } else if (row.cells[2].innerText === "f") {
+      row.classList.add("female");
+    }
+    if (row.cells[1].innerText < 18) {
+      row.style.textDecoration = "line-through";
+    }
+  }
 }
